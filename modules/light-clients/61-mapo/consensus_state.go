a833675457520b1541ed07c18d4923b5354f4773
neoiss <neoiss@163.com>
2023-02-16 20:02:06 +0800

    add consensus state

---
+++ modules/light-clients/61-mapo/consensus_state.go
@@ -0,0 +1,43 @@
+package mapo
+
+import (
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	clienttypes "github.com/cosmos/ibc-go/v7/modules/core/02-client/types"
+	commitmenttypes "github.com/cosmos/ibc-go/v7/modules/core/23-commitment/types"
+	"github.com/cosmos/ibc-go/v7/modules/core/exported"
+)
+
+// ClientType returns MAPO
+func (cs ConsensusState) ClientType() string {
+	return MAPO
+}
+
+// GetRoot returns the commitment Root for the specific
+func (cs ConsensusState) GetRoot() exported.Root {
+	return commitmenttypes.MerkleRoot{Hash: cs.CommitmentRoot}
+}
+
+// GetTimestamp returns block time in nanoseconds of the header that created consensus state
+func (cs ConsensusState) GetTimestamp() uint64 {
+	return uint64(cs.Timestamp.UnixNano())
+}
+
+// ValidateBasic defines a basic validation for the IBFT consensus state.
+func (cs ConsensusState) ValidateBasic() error {
+	if cs.Epoch <= 0 {
+		return sdkerrors.Wrap(clienttypes.ErrInvalidConsensus, "epoch must be greater than 0")
+	}
+	if len(cs.Validators.PairKeys) < 1 {
+		return sdkerrors.Wrap(clienttypes.ErrInvalidConsensus, "pairKeys length must be greater than 0")
+	}
+	if len(cs.Validators.PairKeys) != len(cs.Validators.Weights) {
+		return sdkerrors.Wrap(clienttypes.ErrInvalidConsensus, "invalid validators")
+	}
+	if len(cs.CommitmentRoot) == 0 {
+		return sdkerrors.Wrap(clienttypes.ErrInvalidConsensus, "commitment root cannot be empty")
+	}
+	if cs.Timestamp.Unix() <= 0 {
+		return sdkerrors.Wrap(clienttypes.ErrInvalidConsensus, "timestamp must be a positive unix time")
+	}
+	return nil
+}
