4b725a8b51b1b25f83523c39acb17a9e21818aec
neoiss <neoiss@163.com>
2023-03-02 13:08:41 +0800

    modify errors.go

--- modules/light-clients/61-mapo/errors.go
+++ modules/light-clients/61-mapo/errors.go
@@ -6,5 +6,9 @@ import (
 
 // IBC MAPO client sentinel errors
 var (
-	ErrInvalidHeader = sdkerrors.Register(ModuleName, 6, "invalid header")
+	ErrInvalidHeader              = sdkerrors.Register(ModuleName, 1, "invalid header")
+	ErrInvalidIstanbulHeaderExtra = sdkerrors.Register(ModuleName, 2, "invalid istanbul header extra-data")
+	ErrInvalidAggregatedSeal      = sdkerrors.Register(ModuleName, 3, "invalid aggregated seal")
+	ErrInsufficientSeals          = sdkerrors.Register(ModuleName, 4, "not enough seals to reach quorum")
+	ErrInvalidSignature           = sdkerrors.Register(ModuleName, 5, "invalid signature")
 )
